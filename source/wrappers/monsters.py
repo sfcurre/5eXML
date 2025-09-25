acd0472a4ee6d4cf896c45a30af07688452265bf
Sean Current <seancurrent527@gmail.com>
2025-09-24 23:05:09 -0400

    Fixed monster naming.

--- source/wrappers/monsters.py
+++ source/wrappers/monsters.py
@@ -3,6 +3,12 @@ from .base import Collection, Element
 class Monster(Element):
     def __init__(self, name, element):
         super().__init__(name, element)
+
+    def get(self):
+        name = self.element.find('name')
+        if ' [2024]' in name.text:
+            name.text = name.text.replace(' [2024]', '')
+        return self.element
     
 class MonsterCollection(Collection):
     def __init__(self):
