c28175a4eb9f3556b77c7428f7ff0fa9e62e13ce
Sean Current <seancurrent527@gmail.com>
2026-09-09 22:06:00 -0400

    Update with Arcana Unleashed.

--- source/source_map.py
+++ source/source_map.py
@@ -24,13 +24,14 @@ SOURCE_ABBREVS = {
     'GotG': (r"Bigby Presents: Glory of the Giants", 4),
     'MoM': (r"Mordenkainen Presents: Monsters of the Multiverse", 6),
     'VGM': (r"Volo's Guide to Monsters", 5),
-    '5e': (r"Player's Handbook \(2014\)", 7),
+    '5e': (r"Player's Handbook \(2014\)", 6),
     '': (r"Player's Handbook \(2024\)", 10),
     'MM': (r"Monster Manual", 9),
     'RotF': (r"Icewind Dale: Rime of the Frostmaiden", 4),
     'TBoMT': (r"The Book of Many Things", 4),
     'FotA': (r"Eberron: Forge of the Artificer", 7),
     'FRHoF': (r"Forgotten Realms: Heroes of Faer", 5),
+    'AU': (r"Arcana Unleashed", 7),
 }
 
 class SourceMap:
